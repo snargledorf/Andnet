3757acc231b1e722e94afbffb9ae8f60b6611f40
Ryan Esteves <snargledorf@gmail.com>
2013-03-04 18:14:55 -0500

    Added FileMode
    Currently FileMode does not support Append or Truncate but does support
    all other modes.

--- src/com/theeste/andnet/IO/File.java
+++ src/com/theeste/andnet/IO/File.java
@@ -1,6 +1,7 @@
 package com.theeste.andnet.IO;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public final class File {
 
@@ -8,10 +9,32 @@ public final class File {
 		throw new AssertionError();
 	}
 	
-	public static FileStream Open(String filename) throws FileNotFoundException {
+	public static FileStream open(String filename, FileMode mode) throws IOException {
 		
 		java.io.File file = new java.io.File(filename);
 		
-		return new FileStream(file);
+		if (file.exists() && file.isFile() == false)
+		{
+			throw new IOException("Path does not point to a File");
+		}
+		
+		FileStream fs = null;
+		
+		switch (mode) {
+			case CreateNew:
+				if (file.exists()) throw new IOException("File already exists");
+			case Create:
+				file.delete(); // Delete the file if it already exists
+				if (file.exists()) throw new IOException("Could not overwrite existing file"); // File wasn't deleted
+			default:
+			case OpenOrCreate:
+				file.createNewFile();
+			case Open:
+				if (file.exists() == false) throw new FileNotFoundException();
+				fs = new FileStream(file);
+				break;
+		}
+		
+		return fs;
 	}
 }
---
+++ src/com/theeste/andnet/IO/FileMode.java
@@ -0,0 +1,8 @@
+package com.theeste.andnet.IO;
+
+public enum FileMode {
+	CreateNew,
+	Create,
+	Open,
+	OpenOrCreate
+}
